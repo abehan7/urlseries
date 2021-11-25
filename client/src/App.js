51f07e5bacc7f5fa8a45a2163cd479c257c8e180
한준규 <hanjk13262@gmail.com>
2021-11-25 23:16:16 +0900

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainPage from "./MainPage.js";
+
+const App = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/MainPage.js
@@ -0,0 +1,111 @@
+import React from "react";
+import "./MainPage.css";
+import { FaSearch } from "react-icons/fa";
+import { BiPaperPlane } from "react-icons/bi";
+import { FiPlusSquare } from "react-icons/fi";
+const MainPage = () => {
+  return (
+    <div>
+      <div className="grid-container">
+        <div className="search-box">
+          <FaSearch />
+        </div>
+        <div className="share-write">
+          <BiPaperPlane />
+          <FiPlusSquare />
+        </div>
+        <div className="Rectangle">
+          <h3>내가 지정한 URL </h3>
+          <div className="text-container">
+            <div className="url">리액트 아이콘스 - 리액트 아이콘들 있는 곳</div>
+            <div className="url">리액트 이미지 넣는 법</div>
+            <div className="url">머신러닝 교과서</div>
+          </div>
+        </div>
+        <div className="Rectangle">
+          <h3>자주 이용하는 URL</h3>
+          <div className="text-container">
+            <div className="url">리액트 아이콘스 - 리액트 아이콘들 있는 곳</div>
+            <div className="url">리액트 이미지 넣는 법</div>
+            <div className="url">머신러닝 교과서</div>
+          </div>
+        </div>
+        <div className="Big_Rect">
+          <h3>전체 URL</h3>
+          <div className="text-three-container">
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+            <div className="T-url">리액트 이미지 넣는 법</div>
+            <div className="T-url">머신러닝 교과서</div>
+            <div className="T-url">
+              리액트 아이콘스 - 리액트 아이콘들 있는 곳
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
