14ec917472297188498a0e8c1c69e8ee9c3bd6e3
한준규 <hanjk13262@gmail.com>
2021-11-26 10:16:57 +0900

    fixed untracked files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
+import { FaSearch } from "react-icons/fa";
+
 const App = () => {
   return (
     <>
       <div className="SearchPage">
         <div className="flex-container">
-          <div className="searchBar">검색창</div>
-          <div className="titleBox">제목 박스</div>
-          <div className="HashBox">해쉬태그 박스</div>
+          <div className="searchBar">
+            <FaSearch />
+          </div>
+          <div className="titleBox">
+            <h3>제목박스 </h3>
+          </div>
+          <div className="HashBox">
+            <h3>해쉬태그 </h3>
+          </div>
         </div>
       </div>
     </>
