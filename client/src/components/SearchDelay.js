abeb08be4637cc1d416ad5207fce9215f0195041
한준규 <hanjk13262@gmail.com>
2021-12-02 19:54:33 +0900

    검색기능 완료

--- client/src/components/SearchDelay.js
+++ client/src/components/SearchDelay.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import "./Test.css";
-import { debounce } from "lodash";
+import { debounce, includes } from "lodash";
 import urls from "../urls.json";
 
 const debounceSomethingFunc = debounce(async (e) => {
@@ -13,10 +13,60 @@ const debounceSomethingFunc = debounce(async (e) => {
   // });
 
   // console.log("called debounceSomethingFunc");
+  if (e.target.value.length === 0) {
+    return;
+  }
   console.log(e.target.value);
   // console.log(document.querySelector(".Search-balloon"));
   const urls2 = await urls.urls;
-  urls2.forEach((val) => {
+
+  var hashFilterd = [];
+  var titleFilterd = [];
+
+  //헐 return 2개 하니까 되네
+
+  hashFilterd = urls2.filter((val) => {
+    return val.hashTags.some((tag) => {
+      return tag.includes(e.target.value);
+    });
+  });
+  console.log("hashFilterd first");
+  console.log(hashFilterd);
+
+  titleFilterd = urls2.filter((val) => {
+    return val.title.includes(e.target.value);
+  });
+  // console.log(titleFilterd);
+  //타이틀 검색어
+  titleFilterd.forEach((val) => {
+    const newDiv = document.createElement("div");
+    newDiv.className = "searched-Stuff";
+    newDiv.innerHTML =
+      `<div class="Searched-url-Id">${val.id}</div>` +
+      `<div class="just-bar"> | </div>` +
+      `<div class="Searched-url-Title">${val.title}</div>`;
+    document.querySelector(".Searched-Stuffs-Container").appendChild(newDiv);
+  });
+
+  //해쉬태그 검색어 중복 삭제
+  var hashFilterd2 = hashFilterd.filter((val) => {
+    return titleFilterd.every((val2) => {
+      console.log("내부");
+      console.log(val.id);
+      console.log(val2.id);
+      if (val.id === val2.id) {
+        console.log(val, val2);
+      }
+      return val.id !== val2.id;
+    });
+  });
+  console.log("hashFilterd second");
+
+  console.log(hashFilterd2);
+  console.log(titleFilterd);
+
+  // 해쉬태그 검색어
+  hashFilterd2.forEach((val) => {
     const newDiv = document.createElement("div");
     newDiv.className = "searched-Stuff";
     newDiv.innerHTML =
