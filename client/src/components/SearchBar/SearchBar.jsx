1ef53d5712984f0b9d84f2e905d6bb2681f4307d
한준규 <hanjk13262@gmail.com>
2022-03-09 18:25:43 +0900

    chore:> 폴더정리

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,34 +1,71 @@
 import React from "react";
+import { IoSearchCircleOutline } from "react-icons/io5";
 import styled from "styled-components";
-const SearchBarEl = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 240px;
-  border-radius: 20px;
-`;
 
-const Input = styled.input`
-  border: 0;
-  outline: 0;
-  width: 100%;
-  height: 100%;
-  padding: 0.5rem 0.4rem;
-  font-size: 1rem;
+const InputSearch = styled.input`
+  height: 35px;
+  width: 35px;
+  border: none;
+  font-size: 15px;
+  outline: none;
+  border-radius: 50%;
+  transition: all 0.5s ease-in-out;
   background-color: transparent;
-  transition: all 0.3s ease-in-out;
+  padding-right: 20px;
+  color: transparent;
+  margin-right: 5px;
+  margin-top: 3.5px;
+
+  :focus {
+    width: 300px;
+    color: #535151;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom: 1px solid rgba(114, 114, 114, 0.5);
+    transition: all 500ms cubic-bezier(0, 0.11, 0.35, 2);
+  }
 `;
 
-const InputWrapper = styled.div`
-  background-color: #fff;
+const BtnSearch = styled.button`
+  width: 50px;
+  height: 50px;
+  border-style: none;
+  font-size: 20px;
+  font-weight: bold;
+  outline: none;
+  cursor: pointer;
+  border-radius: 50%;
+  position: absolute;
+  right: 0px;
+  color: #ffffff;
+  background-color: transparent;
+  pointer-events: painted;
+  :focus ~ input {
+    color: #535151;
+
+    width: 300px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom: 1px solid rgba(78, 76, 76, 0.5);
+    transition: all 500ms cubic-bezier(0, 0.11, 0.35, 2);
+  }
 `;
+
+const SearchBarEl = styled.div`
+  width: fit-content;
+  height: fit-content;
+  position: relative;
+  margin-right: 2%;
+  margin-top: 1%;
+`;
+
 const SearchBar = () => {
   return (
     <SearchBarEl>
-      <InputWrapper>
-        <Input />
-      </InputWrapper>
+      <BtnSearch>
+        <IoSearchCircleOutline size="35px" color="gray" />
+      </BtnSearch>
+      <InputSearch type="text" />
     </SearchBarEl>
   );
 };
