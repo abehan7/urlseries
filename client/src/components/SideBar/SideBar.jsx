70754e24ebcb37d9202230a6c7a94c17f46c100f
한준규 <hanjk13262@gmail.com>
2022-03-11 18:44:43 +0900

    feat:> merge success

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -29,8 +29,8 @@ const SideBarEl = styled.div`
   }
 
   z-index: 2;
-  height: 100%;
-  min-height: 650px;
+  height: calc(100vh - 100px);
+  /* min-height: 650px; */
   width: 180px;
   background-color: #fff;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
